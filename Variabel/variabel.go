98652941c2ba6a30ad487d4215a78b3e1d8e6600
Dava aditya perdana <adava7132@gmail.com>
2024-11-21 15:47:15 +0700

    add-variabel

--- Variabel/variabel.go
+++ Variabel/variabel.go
@@ -7,4 +7,11 @@ var pl = fmt.Println
 func main() {
 	pl("Belajar variabel")
 
+	var name string = "Dava aditya perdana"
+	var umur uint8 = 19
+	var address string = "Medan"
+	pl("Nama saya adalah :", name)
+	pl("Umur saya adalah :", umur)
+	pl("Alamat saya adalah :", address)
+
 }
