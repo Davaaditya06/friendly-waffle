842dbbbc8c3242dc382b150de579ada6f74f6725
Dava aditya perdana <adava7132@gmail.com>
2024-11-21 15:51:26 +0700

    add-tipe-data

---
+++ Tipe-data/tipe-data.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Belajar Tipe data")
+
+	// string
+	var name string
+	name = "Ucup"
+	// integer
+	var umur uint8
+	umur = 18
+	// float
+	var gaji float64
+	gaji = 20.0000
+
+	// boolean
+	var menikah bool
+	menikah = false
+
+	fmt.Println("Nama :", name)
+	fmt.Println("Umur :", umur)
+	fmt.Println("Gaji :", gaji)
+	fmt.Println("Menikah :", menikah)
+
+}
